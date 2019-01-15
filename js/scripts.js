764c5d607c8e6ba3567e22a77d48ef48ce4f5551
fl <fen.lombardi@gmail.com>
2019-01-14 20:04:07 -0800

    Add 2nd pop up to header script, move scripts html from body to head.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
+//jquery is a function, .click is a method that attaches an event handler to the selected elements. Event handlers listen to the element and respond when clicked. Using a function as an argument and not defining a variable makes it an annonymous callback function (when a function is passed to another function as an argument). Callback function is not executed immediatly but instead after the click function is executed.
+
 jQuery("h1").click(function() {
   alert("This is a header.");
+  alert("I told you, THIS IS A HEADER!");
 });
 
 jQuery("p").click(function() {
